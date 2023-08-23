e0b11543a935f59b4847635071e1a14e2b6a6d70
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-08-23 10:14:14 +0530

    Update repo

--- bin/cli.js
+++ bin/cli.js
@@ -13,7 +13,7 @@ const runCommand = command => {
 }
 
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/entapex/entapex-app ${repoName}`
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/shreyashrangrej/entapex-app ${repoName}`
 const installDepsCommand = `cd ${repoName} && npm install`
 
 console.log(`Cloning the repository with name ${repoName}`);
