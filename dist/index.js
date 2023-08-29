1d299841bc3cf3063d57eb3419a532cf26a26866
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-08-29 15:29:02 +0530

    update

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-console.log("Hello EntApex CLI");
+console.log("Hello EntApex CLI 2");
 export {};
 //# sourceMappingURL=index.js.map
--- src/cli/index.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-import fs from "fs";
-console.log("Hello EntApex CLI")
