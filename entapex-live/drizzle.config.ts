5ed22c53472cf59df461295f2bba25b11050d834
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-10-03 11:26:53 +0530

    Update dizzle File structure

--- entapex-live/drizzle.config.ts
+++ entapex-live/drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "drizzle-kit";
+import "dotenv/config"
 
 export default {
-  schema: "./src/lib/db/schema.ts",
+  schema: "./src/lib/db/schema",
   out: "./src/lib/db/migrations",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || "",
+    connectionString: process.env.DATABASE_URL || ""
   },
   driver: "pg",
 } satisfies Config;
